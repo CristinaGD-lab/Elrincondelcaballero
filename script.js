71661533a2755ca4dd18e05e1df1b30f83ac4d44
Cristina González Domínguez <cristina.gonzalez92@hotmail.com>
2025-03-02 21:56:35 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-//Mostrar mensaje al entrar en la página
 
-document.addEventListener("DOMContentLoaded", function () {
-  alert("¡Bienvenido a mi página web!");
-});
+
+// Función para mostrar la sección seleccionada y ocultar las demás
 function mostrarSeccion(id) {
   // Ocultar todas las secciones
   let secciones = document.querySelectorAll(".seccion");
   secciones.forEach((seccion) => {
-    seccion.classList.remove("activa");
+    seccion.classList.remove("activa"); // Elimina la clase 'activa' de todas las secciones
   });
 
   // Mostrar la sección seleccionada
-  document.getElementById(id).classList.add("activa");
+  document.getElementById(id).classList.add("activa"); // Agrega la clase 'activa' a la sección seleccionada
 }
